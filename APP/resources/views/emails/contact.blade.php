297c2d3ca779c0b2cb88ba9d2693473667982c1f
samir20-23 <germanysamir1@gmail.com>
2024-10-24 15:40:53 +0100

    new

---
+++ APP/resources/views/emails/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nouveau message de contact</title>
+</head>
+<body>
+    <h1>Nouveau message de contact</h1>
+    <p><strong>Nom:</strong> {{ $name }}</p>
+    <p><strong>Email:</strong> {{ $email }}</p>
+    <p><strong>Message:</strong></p>
+    <p>{{ $message }}</p>
+</body>
+</html>
