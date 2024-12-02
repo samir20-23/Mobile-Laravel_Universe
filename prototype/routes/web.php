abcfe9cd4a78142b152c91b7935c9ab8ded730e7
samir20-23 <germanysamir1@gmail.com>
2024-12-02 10:05:24 +0000

    new

--- prototype/routes/web.php
+++ prototype/routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('contact');
+use Illuminate\Support\Facades\Route;
+
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+
+Route::middleware('auth.middleware')->group(function () {
+    Route::get('/dashboard', [\App\Http\Controllers\AdminController::class, 'dashboard'])->name('dashboard');
 });
-Route::get('/contact', [ContactController::class, 'create']);
-Route::post('/contact', [ContactController::class, 'store']);
